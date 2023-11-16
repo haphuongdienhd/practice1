1803ad34783b76ff24675b20a3b90ad5549a1b7e
haphuongdienhd <dien.haphuong@hdwebsoft.dev>
2023-11-16 15:06:09 +0700

    custom paginator

---
+++ practice1/pagination.py
@@ -0,0 +1,22 @@
+from math import ceil
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
+
+
+DEFAULT_PAGE = 1
+DEFAULT_PAGE_SIZE = 10
+
+class CustomPagination(PageNumberPagination):
+    page = DEFAULT_PAGE
+    page_size = DEFAULT_PAGE_SIZE
+    page_size_query_param = 'page_size'
+
+    def get_paginated_response(self, data):
+        return Response({
+            'next': self.get_next_link(),
+            'previous': self.get_previous_link(),            
+            'total': ceil(int(self.page.paginator.count)/self.page_size),
+            'page': int(self.request.GET.get('page', DEFAULT_PAGE)), # can not set default = self.page
+            'page_size': int(self.request.GET.get('page_size', self.page_size)),
+            'results': data
+        })
