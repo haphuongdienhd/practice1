a19e31092351e4ec69179978089ce5e26281a0d0
haphuongdienhd <dien.haphuong@hdwebsoft.dev>
2023-11-27 09:58:31 +0700

    settings changes

--- practice1/settings.py
+++ practice1/settings.py
@@ -58,7 +58,7 @@ EXTERNAL_APPS = [
     'health_checks',
 ]
 
-INSTALLED_APPS = INSTALLED_APPS + MY_APPS + EXTERNAL_APPS
+INSTALLED_APPS = MY_APPS + INSTALLED_APPS + EXTERNAL_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -127,6 +127,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = 'user.MyUser'
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
@@ -175,7 +176,7 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.BasicAuthentication',
-        'user.authentication.ExpiringTokenAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',
     ],
 }
