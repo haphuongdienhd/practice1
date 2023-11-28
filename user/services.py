aa1be817ecbe8ca519e1d074d8672b89de6ca206
haphuongdienhd <dien.haphuong@hdwebsoft.dev>
2023-11-28 11:04:11 +0700

    exceptions to sevices

--- user/services.py
+++ user/services.py
@@ -3,7 +3,9 @@ import pytz
 
 from django.conf import settings
 from django.utils import timezone
-from user.models import MyUser
+
+from .models import MyUser
+from .exceptions import *
 
 from rest_framework.authtoken.models import Token
 
@@ -14,28 +16,28 @@ def find_token_by_user_id(user_id):
     try:
         return Token.objects.get(user_id=user_id)
     except Token.DoesNotExist:
-        return None
+        raise ObjectNotFound(TokenObject(), 'user_id', user_id)
     
 def find_user_by_id(id):
     
     try:
         return MyUser.objects.get(pk=id)
     except MyUser.DoesNotExist:
-        return None
+        return ObjectNotFound(UserObject(), 'id', id)
 
 def find_user_by_name(name):
     
     try:
         return MyUser.objects.select_related().get(username=name)
     except MyUser.DoesNotExist:
-        return None
+        return ObjectNotFound(UserObject(), 'name', name)
     
 def find_user_by_email(email):
     
     try:
         return MyUser.objects.get(email=email)
     except MyUser.DoesNotExist:
-        return None
+        return ObjectNotFound(UserObject(), 'email', email)
 
 def token_expire(token_created):
     """Return True if Token has not been expired
